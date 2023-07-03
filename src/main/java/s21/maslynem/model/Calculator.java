1a81baaa607adde52c45d1cde0cd5eeda0f96ee0
maslynem <maslynem@student.21-school.ru>
2023-07-03 10:37:21 +0300

    Refactor Calculator.java

--- src/main/java/s21/maslynem/model/Calculator.java
+++ src/main/java/s21/maslynem/model/Calculator.java
@@ -3,167 +3,42 @@ package s21.maslynem.model;
 import java.util.*;
 
 public class Calculator {
-    private static final String OPERATORS = "%*+-/^";
-    private static final String[] FUNCTIONS = {"acos", "asin", "atan", "cos", "ctg", "ln", "log", "sin", "sqrt", "tan"};
-    private static final String OPEN_BRACKET = "(";
-    private static final String CLOSE_BRACKET = ")";
 
+    private Calculator() {}
     public static double calculate(String expression) {
-        String newExpression = prepareExpression(expression);
-        List<String> postfix = castToPostfix(newExpression);
+        List<String> postfix = FromInfixToPostfixTransformer.transform(expression);
         return calculateByPostfix(postfix);
-    }
-
-    private static String prepareExpression(String expression) {
-        String newExpression = expression
-                .toLowerCase()
-                .replaceAll(" ", "")
-                .replaceAll("π", String.valueOf(Math.PI))
-                .replaceAll("√", "sqrt")
-                .replaceAll(",", ".")
-                .replaceAll("\\(-", "(0-")
-                .replaceAll("\\.-", ".0-")
-                .replaceAll("\\(\\+", "(");
-        char ch = newExpression.charAt(0);
-        if (ch == '-' || ch == '+') {
-            newExpression = "0" + newExpression;
-        }
-        return newExpression;
-    }
-
-    private static List<String> castToPostfix(String expression) {
-        List<String> postfix = new ArrayList<>();
-        Stack<String> stack = new Stack<>();
-        StringTokenizer tokens = new StringTokenizer(expression, OPERATORS + "()e", true);
-        while (tokens.hasMoreTokens()) {
-            String token = tokens.nextToken();
-            if (token.equals("e")) {
-                String operand = postfix.get(postfix.size() - 1);
-                postfix.remove(postfix.size() - 1);
-                postfix.add(operand + token + tokens.nextToken() + tokens.nextToken());
-            } else if (isOperand(token)) {
-                postfix.add(token);
-            } else if (isFunction(token)) {
-                stack.push(token);
-            } else if (isOperator(token)) {
-                while (!stack.empty() && isOperator(stack.peek()) &&
-                        (getOperatorPriority(stack.peek()) > getOperatorPriority(token) ||
-                                getOperatorPriority(stack.peek()) == getOperatorPriority(token) && token.equals("^"))) {
-                    postfix.add(stack.pop());
-                }
-                stack.push(token);
-            } else if (isOpenBracket(token)) {
-                stack.push(token);
-            } else if (isCloseBracket(token)) {
-                try {
-                    while (!isOpenBracket(stack.peek())) {
-                        postfix.add(stack.pop());
-                    }
-                    stack.pop();
-                    if (!stack.empty() && isFunction(stack.peek())) {
-                        postfix.add(stack.pop());
-                    }
-                } catch (EmptyStackException exception) {
-                    throw new WrongExpressionException("Wrong count of brackets.");
-                }
-            } else {
-                throw new WrongExpressionException(String.format("[%s] is not available.", token));
-            }
-        }
-        while (!stack.empty()) {
-            String token = stack.pop();
-            if (isOpenBracket(token)) {
-                throw new WrongExpressionException("Wrong count of brackets.");
-            }
-            postfix.add(token);
-        }
-        return postfix;
     }
 
     private static double calculateByPostfix(List<String> postfix) {
         try {
-            Stack<Double> stack = new Stack<>();
+            Deque<Double> stack = new ArrayDeque<>();
             for (String token : postfix) {
-                if (isOperand(token)) {
-                    double value = Double.parseDouble(token);
-                    if (Math.abs(0 - value) < 1e-7) {
-                        value = 0;
-                    }
-                    stack.push(value);
-                } else if (isOperator(token)) {
-                    double result = executeOperator(stack, token);
-                    stack.push(result);
-                } else if (isFunction(token)){
-                    double result = executeFunction(stack, token);
-                    stack.push(result);
+                if (StringUtils.isOperand(token)) {
+                    stack.push(Double.parseDouble(token));
+                } else if (StringUtils.isOperator(token)) {
+                    stack.push(executeOperator(stack, token));
+                } else if (StringUtils.isFunction(token)){
+                    stack.push(executeFunction(stack, token));
                 } else {
                     throw new WrongExpressionException("Wrong expression: undefined token [" + token +"]");
                 }
             }
             double result = stack.pop();
-            if (!stack.empty()) {
+            if (!stack.isEmpty()) {
                 throw new WrongExpressionException("Wrong expression: missing operator");
             }
             return result;
-        } catch (EmptyStackException exception) {
+        } catch (NoSuchElementException exception) {
             throw new WrongExpressionException("Wrong expression: unexpected operator");
         }
     }
 
-    private static boolean isOperand(String string) {
-        try {
-            Double.parseDouble(string);
-            return true;
-        } catch (NumberFormatException exception) {
-            return false;
-        }
-    }
 
-    private static boolean isOperator(String string) {
-        return string.length() == 1 && OPERATORS.contains(string);
-    }
 
-    private static boolean isOpenBracket(String string) {
-        return OPEN_BRACKET.equals(string);
-    }
 
-    private static boolean isCloseBracket(String string) {
-        return CLOSE_BRACKET.equals(string);
-    }
 
-    private static boolean isFunction(String string) {
-        return Arrays.binarySearch(FUNCTIONS, string) >= 0;
-    }
-
-    private static int getOperatorPriority(String operator) {
-        switch (operator) {
-            case "+":
-            case "-":
-                return 1;
-            case "*":
-            case "/":
-            case "%":
-                return 2;
-            case "^":
-                return 3;
-            case "acos":
-            case "asin":
-            case "atan":
-            case "cos":
-            case "ctg":
-            case "ln":
-            case "log":
-            case "mod":
-            case "sin":
-            case "sqrt":
-            case "tan":
-                return 4;
-            default:
-                return -1;
-        }
-    }
-
-    private static double executeOperator(Stack<Double> stack, String operator) {
+    private static double executeOperator(Deque<Double> stack, String operator) {
         double secondOperand = stack.pop();
         double firstOperand = stack.pop();
         switch (operator) {
@@ -183,7 +58,7 @@ public class Calculator {
         return Double.POSITIVE_INFINITY;
     }
 
-    private static double executeFunction(Stack<Double> stack, String function) {
+    private static double executeFunction(Deque<Double> stack, String function) {
         double operand = stack.pop();
         switch (function) {
             case "acos":
---
+++ src/main/java/s21/maslynem/model/FromInfixToPostfixTransformer.java
@@ -0,0 +1,120 @@
+package s21.maslynem.model;
+
+import java.util.*;
+
+class FromInfixToPostfixTransformer {
+
+    private FromInfixToPostfixTransformer() {
+    }
+
+    static List<String> transform(String expression) {
+        String newExpression = prepareExpression(expression);
+        return castToPostfix(newExpression);
+    }
+
+    private static String prepareExpression(String expression) {
+        String newExpression = expression
+                .toLowerCase()
+                .replace(" ", "")
+                .replace("π", String.valueOf(Math.PI))
+                .replace("√", "sqrt")
+                .replace(",", ".")
+                .replace("(-", "(0-")
+                .replace(".-", ".0-")
+                .replace("(+", "(");
+        char ch = newExpression.charAt(0);
+        if (ch == '-' || ch == '+') {
+            newExpression = "0" + newExpression;
+        }
+        return newExpression;
+    }
+
+    private static List<String> castToPostfix(String expression) {
+        List<String> postfix = new ArrayList<>();
+        Deque<String> stack = new ArrayDeque<>();
+        StringTokenizer tokens = new StringTokenizer(expression, "%*+-/^()e", true);
+        while (tokens.hasMoreTokens()) {
+            String token = tokens.nextToken();
+            if (token.equals("e")) {
+                handleExpCase(token, tokens.nextToken(), postfix);
+            } else if (StringUtils.isOperand(token)) {
+                postfix.add(token);
+            } else if (StringUtils.isFunction(token)) {
+                stack.push(token);
+            } else if (StringUtils.isOperator(token)) {
+                handleOperatorCase(token, stack, postfix);
+            } else if (StringUtils.isOpenBracket(token)) {
+                stack.push(token);
+            } else if (StringUtils.isCloseBracket(token)) {
+                handleCloseBracketCase(stack, postfix);
+            } else {
+                throw new WrongExpressionException(String.format("[%s] is not available.", token));
+            }
+        }
+        while (!stack.isEmpty()) {
+            String token = stack.pop();
+            if (StringUtils.isOpenBracket(token)) {
+                throw new WrongExpressionException("Wrong count of brackets.");
+            }
+            postfix.add(token);
+        }
+        return postfix;
+    }
+
+    private static void handleExpCase(String token, String nextToken, List<String> postfix) {
+        String operand = postfix.get(postfix.size() - 1);
+        postfix.remove(postfix.size() - 1);
+        postfix.add(operand + token + nextToken + nextToken);
+    }
+
+    private static void handleOperatorCase(String token, Deque<String> stack, List<String> postfix) {
+        while (!stack.isEmpty() && StringUtils.isOperator(stack.peek()) &&
+                (getOperatorPriority(stack.peek()) > getOperatorPriority(token) ||
+                        getOperatorPriority(stack.peek()) == getOperatorPriority(token) && token.equals("^"))) {
+            postfix.add(stack.pop());
+        }
+        stack.push(token);
+    }
+
+    private static void handleCloseBracketCase(Deque<String> stack, List<String> postfix) {
+        try {
+            while (!StringUtils.isOpenBracket(stack.peek())) {
+                postfix.add(stack.pop());
+            }
+            stack.pop();
+            if (StringUtils.isFunction(stack.peek())) {
+                postfix.add(stack.pop());
+            }
+        } catch (NoSuchElementException exception) {
+            throw new WrongExpressionException("Wrong count of brackets.");
+        }
+    }
+
+    private static int getOperatorPriority(String operator) {
+        switch (operator) {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+            case "%":
+                return 2;
+            case "^":
+                return 3;
+            case "acos":
+            case "asin":
+            case "atan":
+            case "cos":
+            case "ctg":
+            case "ln":
+            case "log":
+            case "mod":
+            case "sin":
+            case "sqrt":
+            case "tan":
+                return 4;
+            default:
+                return -1;
+        }
+    }
+}
